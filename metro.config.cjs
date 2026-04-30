2ae7cb1fcb3625b24d63d46321741ae8caa4162e
antoniocristovam <antoniocristovam@outlook.com.br>
2026-04-30 11:54:30 -0300

    Refactor code structure for improved readability and maintainability

---
+++ metro.config.cjs
@@ -0,0 +1,9 @@
+const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, {
+  input: path.resolve("./src/styles/global.css"),
+});
