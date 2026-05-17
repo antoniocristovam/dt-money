26247e08466fa36ce635011f464dbb481d22a2df
antoniocristovam <antoniocristovam@outlook.com.br>
2026-05-17 10:43:09 -0300

    Add PublicStackParamsList type and implement Home and Login screens with navigation

---
+++ src/routes/publicRoutes/types-public-routes.ts
@@ -0,0 +1,4 @@
+export type PublicStackParamsList = {
+  Login: undefined;
+  Register: undefined;
+};
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,11 +3,13 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { Text, TouchableOpacity, View } from "react-native";
 
-export const LoginScreen = () => {
+export const Home = () => {
   const navigator = useNavigation<StackNavigationProp<PublicStackParamsList>>();
+  console.log("TELA DE HOME");
+
   return (
     <View className="flex-1 items-center justify-center">
-      <Text>Tela de login</Text>
+      <Text>Tela de home</Text>
       <TouchableOpacity
         className="bg-green-500 px-4 py-2 rounded"
         onPress={() => navigator.navigate("Register")}
